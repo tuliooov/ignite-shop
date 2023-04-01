561b8a452d417eaa85df064b4d5c36856b18aca3
Marco T O Valeriano <marco.valeriano@bancointer.com.br>
2023-04-01 01:19:09 -0300

    test searc params

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -12,48 +12,23 @@ export const metadata = {
 
 interface SuccessProps {
   searchParams: {
-    session_id: string
+    // session_id: string
   }
 }
 
-export default async function Success({ searchParams: { session_id = '' } }: SuccessProps) {
+export default async function Success({ searchParams = {} }: SuccessProps) {
 
-  const sessionId = String(session_id);
-  const {costumerName, product, failed} = createSession(await stripe.checkout?.sessions?.retrieve(sessionId, {
-    expand: ['line_items', 'line_items.data.price.product']
-  }).then((response) => {
-    return response
-  }).catch((response) => {
-    return { ...response, failed: true}
-  }))
+  // const sessionId = String(session_id);
+  // const {costumerName, product, failed} = createSession(await stripe.checkout?.sessions?.retrieve(sessionId, {
+  //   expand: ['line_items', 'line_items.data.price.product']
+  // }).then((response) => {
+  //   return response
+  // }).catch((response) => {
+  //   return { ...response, failed: true}
+  // }))
 
 
-  if(failed || !product || !costumerName){
     return <>
-    <p>ops</p></>
-  }
+    <p>{JSON.stringify(searchParams)}</p></>
 
-  return (
-    <>
-      {/* <Head>
-        <meta name="robots" content="noindex" />
-      </Head> */}
-
-      <SuccessContainer>
-        <h1>Compra efetuada</h1>
-
-        <ImageContainer>
-          <Image src={product.imageUrl} width={150} height={110} alt="" />
-        </ImageContainer>
-
-        <p>
-          Uhuul <strong>{costumerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
-        </p>
-
-        <Link href="/">
-          Voltar ao catálogo
-        </Link>
-      </SuccessContainer>
-    </>
-  )
 }
